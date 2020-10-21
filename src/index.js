846057ffb727cfaa5cdc4f44a458613cdf74662d
irabudkova <thehive@list.ru>
2020-10-21 19:51:07 +0300

    feat: implement morse-decoder

--- src/index.js
+++ src/index.js
@@ -1,46 +1,75 @@
-const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+let MORSE_TABLE = {
+  '**********': ' ',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+  let result = '';
+  let keys = Object.keys(MORSE_TABLE);
+
+  let decodeTable = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+
+    let keyDecodeArr = key.split('').map(x => {
+      if (x === '-') {
+        return '11';
+      } else if (x === '.') {
+        return '10';
+      } else {
+        return x;
+      }
+    });
+
+    let decodeKey = keyDecodeArr.join('').padStart(10, '0');
+    decodeTable[decodeKey] = MORSE_TABLE[key];
+  }
+
+  for (let i = 0; i < expr.length / 10; i++) {
+    let encodedChar = expr.slice(10 * i, 10 * i + 10);
+    const decodedChar = decodeTable[encodedChar];
+    result += decodedChar;
+  }
+  
+  return result;
 }
 
 module.exports = {
-    decode
+  decode
 }
